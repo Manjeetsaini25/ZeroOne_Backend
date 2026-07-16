638c5911ac476683b9543340822e77c435e58270
Manjeetsaini25 <manjeetsaini2003.com@gmail.com>
2026-07-16 11:24:46 +0530

    Backend Initialization - 01

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+require('dotenv').config();
+const main =  require('./config/db')
+const cookieParser =  require('cookie-parser');
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+main()
+.then(async ()=>{
+    app.listen(process.env.PORT, ()=>{
+        console.log("Server listening at port number: "+ process.env.PORT);
+    })
+})
+.catch(err=> console.log("Error Occurred: "+err));
+
+// app.get('/',(req,res)=>{
+//     res.send("hii")
+// })
+
+//  app.listen(process.env.PORT, ()=>{
+//         console.log("Server listening at port number: "+ process.env.PORT);
+//  })

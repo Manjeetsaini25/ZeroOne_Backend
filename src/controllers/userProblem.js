c4357cb173eaff6f660133aaa14d2e5e9fa82eb7
Manjeetsaini25 <manjeetsaini2003.com@gmail.com>
2026-07-19 09:44:55 +0530

    implement update & delete problem functionality - 07

--- src/controllers/userProblem.js
+++ src/controllers/userProblem.js
@@ -92,4 +92,94 @@ const createProblem = async (req, res) => {
     }
 };
 
-module.exports = createProblem;
+const updateProblem = async (req,res)=>{
+    
+  const {id} = req.params;
+  const {title,description,difficulty,tags,
+    visibleTestCases,hiddenTestCases,startCode,
+    referenceSolution, problemCreator
+   } = req.body;
+
+  try{
+
+     if(!id){
+      return res.status(400).send("Missing ID Field");
+     }
+
+    const DsaProblem =  await Problem.findById(id);
+    if(!DsaProblem)
+    {
+      return res.status(404).send("ID is not persent in server");
+    }
+      
+    for(const {language,completeCode} of referenceSolution){
+         
+
+      // source_code:
+      // language_id:
+      // stdin: 
+      // expectedOutput:
+
+      const languageId = getLanguageById(language);
+        
+      // I am creating Batch submission
+      const submissions = visibleTestCases.map((testcase)=>({
+          source_code:completeCode,
+          language_id: languageId,
+          stdin: testcase.input,
+          expected_output: testcase.output
+      }));
+
+
+      const submitResult = await submitBatch(submissions);
+      // console.log(submitResult);
+
+      const resultToken = submitResult.map((value)=> value.token);
+
+      // ["db54881d-bcf5-4c7b-a2e3-d33fe7e25de7","ecc52a9b-ea80-4a00-ad50-4ab6cc3bb2a1","1b35ec3b-5776-48ef-b646-d5522bdeb2cc"]
+      
+     const testResult = await submitToken(resultToken);
+
+    //  console.log(testResult);
+
+     for(const test of testResult){
+      if(test.status_id!=3){
+       return res.status(400).send("Error Occured");
+      }
+     }
+
+    }
+
+
+  const newProblem = await Problem.findByIdAndUpdate(id , {...req.body}, {runValidators:true, new:true});
+   
+  res.status(200).send(newProblem);
+  }
+  catch(err){
+      res.status(500).send("Error: "+err);
+  }
+}
+
+const deleteProblem = async(req,res)=>{
+
+  const {id} = req.params;
+  try{
+     
+    if(!id)
+      return res.status(400).send("ID is Missing");
+
+   const deletedProblem = await Problem.findByIdAndDelete(id);
+
+   if(!deletedProblem)
+    return res.status(404).send("Problem is Missing");
+
+
+   res.status(200).send("Successfully Deleted");
+  }
+  catch(err){
+     
+    res.status(500).send("Error: "+err);
+  }
+}
+
+module.exports = {createProblem,updateProblem,deleteProblem};
--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const problemRouter =  express.Router();
 const adminMiddleware = require("../middleware/adminMiddleware")
-const createProblem = require("../controllers/userProblem");
+const {createProblem,updateProblem,deleteProblem} = require("../controllers/userProblem");
+const userMiddleware = require("../middleware/userMiddleware");
 
 // Create
 problemRouter.post("/create",adminMiddleware ,createProblem);
-// problemRouter.patch("/:id", updateProblem);
-// problemRouter.delete("/:id",deleteProblem);
+problemRouter.put("/update/:id",adminMiddleware, updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware, deleteProblem);
 
 
 // problemRouter.get("/:id",getProblemById);

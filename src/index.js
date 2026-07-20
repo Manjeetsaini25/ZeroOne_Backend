2a8742db67d6d627bb03e74ab8b20ee39ade5deb
Manjeetsaini25 <manjeetsaini2003.com@gmail.com>
2026-07-20 10:12:29 +0530

    implement submit & run controller - 09

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const redisClient = require('./config/redis');
 const cookieParser =  require('cookie-parser');
 const authRouter = require("./routes/userAuth");
 const problemRouter = require("./routes/problemCreator");
+const submitRouter = require("./routes/submit")
 
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/user',authRouter);
 app.use('/problem',problemRouter);
+app.use('/submission',submitRouter);
 
 const InitalizeConnection = async ()=>{
     try{

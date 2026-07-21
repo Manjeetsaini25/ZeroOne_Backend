e00816ba20c29a65b73141092edc86f2c0e850a7
Manjeetsaini25 <manjeetsaini2003.com@gmail.com>
2026-07-21 07:23:39 +0530

    apply rate limiter on submit code - 14

--- src/routes/submit.js
+++ src/routes/submit.js
@@ -3,8 +3,9 @@ const express = require('express');
 const submitRouter = express.Router();
 const userMiddleware = require("../middleware/userMiddleware");
 const {submitCode,runCode} = require("../controllers/userSubmission");
+const submitCodeRateLimiter = require("../config/rateLimiter")
 
-submitRouter.post("/submit/:id", userMiddleware, submitCode);
+submitRouter.post("/submit/:id", userMiddleware,submitCodeRateLimiter, submitCode);
 submitRouter.post("/run/:id",userMiddleware,runCode);
 
 module.exports = submitRouter;

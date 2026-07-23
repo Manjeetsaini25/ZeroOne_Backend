f8a5997947056a011d804aa307e689d4dbc3c305
Manjeetsaini25 <manjeetsaini2003.com@gmail.com>
2026-07-23 15:41:38 +0530

    resolve cors issue & user schema's problem solved field issue - 17

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ const cookieParser =  require('cookie-parser');
 const authRouter = require("./routes/userAuth");
 const problemRouter = require("./routes/problemCreator");
 const submitRouter = require("./routes/submit")
+const cors = require('cors')
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true 
+}))
 
 app.use(express.json());
 app.use(cookieParser());
